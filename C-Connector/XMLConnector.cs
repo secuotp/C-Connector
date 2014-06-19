1a656cf84bac6c6a97e3a4fb4bde09b246c3dffe
polwath <polwath_7@hotmail.com>
2014-06-19 15:21:44 +0900

    Updtae

--- C-Connector/XMLConnector.cs
+++ C-Connector/XMLConnector.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Linq;
 using System.Text;
 using System.Xml;
@@ -16,23 +15,9 @@ namespace C_Connector
         {
             XmlDocument xmld = new XmlDocument();
             xmld.LoadXml(string.Format("<root>{0}</root>",xml));
-            //runasync(xmld).Wait();
 
+            WebRequest req = null;
 
         }
-
-        /*public static async Task runasync(XmlDocument xml)
-        {
-            using(var client = new HttpClient()){
-                client.BaseAddress = new Uri("https://secuotp.sit.kmutt.ac.th/");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-
-                var send = xml;
-                HttpResponseMessage response = await client.GetAsync("SecuOTP-Service/manage/register/end-user");
-                response = await client.PostAsync();
-
-            }
-        }*/
     }
 }
