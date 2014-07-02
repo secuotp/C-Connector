288d6e389c73f1327f6b65c3ea3573efc485d090
polwath <polwath_7@hotmail.com>
2014-07-02 17:09:22 +0900

    Most Update

--- C-Connector/Get.cs
+++ C-Connector/Get.cs
@@ -8,10 +8,22 @@ namespace C_Connector
 {
     class Get
     {
-        public string createGetRequest(string username, string type){
+        public string createGetRequest(string domain, string serial, string username, string type){
             if(type.Equals("full") || type.Equals("default")){
                 string service = "U-01";
-                
+
+                XMLParameter authen = new XMLParameter();
+                XMLParameter param = new XMLParameter();
+                CreateXML doc = new CreateXML();
+
+                authen.Add("domain", domain);
+                authen.Add("serial", serial);
+                param.Add("username",username);
+                param.Add("type",type);
+
+                string result = doc.createRequest(service,authen,param);
+
+                return result;
             }
             else
             {
