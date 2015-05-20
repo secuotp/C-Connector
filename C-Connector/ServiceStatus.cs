bce944ca0427de5d8b61a882e18128d602e68d67
polwath <polwath_7@hotmail.com>
2015-05-20 20:44:28 +0900

    Re-commit missing file: ServiceStatus.cs

---
+++ C-Connector/ServiceStatus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Connector //
+{
+    public class ServiceStatus
+    {
+        private int statusId;
+        private String statusText;
+        private object data;
+
+        public ServiceStatus(int statusId, String statusText)
+        {
+            this.statusId = statusId;
+            this.statusText = statusText;
+        }
+
+        public int getStatusId()
+        {
+            return statusId;
+        }
+
+        public void setStatusId(int statusId)
+        {
+            this.statusId = statusId;
+        }
+
+        public String getStatusText()
+        {
+            return statusText;
+        }
+
+        public void setStatusText(String statusText)
+        {
+            this.statusText = statusText;
+        }
+
+        public object getData()
+        {
+            return data;
+        }
+
+        public void setData(object data)
+        {
+            this.data = data;
+        }
+    }
+}
